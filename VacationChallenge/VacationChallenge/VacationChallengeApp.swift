d755b489c459b6c0d0809bfa97d0898bb79ffe4c
Agatha Santos <agathabsantos2@gmail.com>
2026-07-20 17:29:24 -0300

    implementacao da primeira tela

---
+++ VacationChallenge/VacationChallenge/VacationChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationChallengeApp.swift
+//  VacationChallenge
+//
+//  Created by Agatha Barbosa Marinho dos Santos on 19/07/26.
+//
+
+import SwiftUI
+
+@main
+struct VacationChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VacationChallenge/VacationChallengeUITests/VacationChallengeUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  VacationChallengeUITestsLaunchTests.swift
+//  VacationChallengeUITests
+//
+//  Created by Agatha Barbosa Marinho dos Santos on 19/07/26.
+//
+
+import XCTest
+
+final class VacationChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
