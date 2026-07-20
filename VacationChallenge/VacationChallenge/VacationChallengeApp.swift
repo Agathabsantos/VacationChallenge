21fc30e1c17327a042442c5d1562f2e027637d1a
Agatha Santos <agathabsantos2@gmail.com>
2026-07-20 17:29:53 -0300

    implementacao da primeira tela

--- VacationChallenge/VacationChallenge/VacationChallengeApp.swift
+++ VacationChallenge/VacationChallenge/VacationChallengeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct VacationChallengeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ConversationsView()
         }
     }
 }
